31e5e65572294654b4c2b8e7e4ecbf8478ddc2d5
Maria Eduarda <marriae@hotmail.com>
2020-09-29 17:13:48 -0300

    JWT Finalizado

--- ProjetoEduX/ProjetoEduX/Controllers/LoginController.cs
+++ ProjetoEduX/ProjetoEduX/Controllers/LoginController.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -32,6 +29,7 @@ namespace ProjetoEduX.Controllers
             _config = config;
         }
 
+        // Chamamos nosso método para validar nosso usuário da aplicação
         private Usuario AuthenticateUser(Usuario login)
         {
             return _context.Usuario.Include(a => a.IdPerfilNavigation).FirstOrDefault(u => u.Email == login.Email && u.Senha == login.Senha);
@@ -46,36 +44,32 @@ namespace ProjetoEduX.Controllers
             // Definimos nossas Claims (dados da sessão) para poderem ser capturadas
             // a qualquer momento enquanto o Token for ativo
             var claims = new[] {
-        new Claim(JwtRegisteredClaimNames.NameId, userInfo.Nome),
-        new Claim(JwtRegisteredClaimNames.Email, userInfo.Email),
-        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-        //new Claim(ClaimTypes.Role, userInfo.IdPerfilNavigation.Permissao)
-
-    };
+                new Claim(JwtRegisteredClaimNames.NameId, userInfo.Nome),
+                new Claim(JwtRegisteredClaimNames.Email, userInfo.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Role, userInfo.IdPerfilNavigation.Permissao)
+            };
 
             // Configuramos nosso Token e seu tempo de vida
-            var token = new JwtSecurityToken
-                (
-                    _config["Jwt:Issuer"],
-                    _config["Jwt:Issuer"],
-                    claims,
-                    expires: DateTime.Now.AddMinutes(120),
-                    signingCredentials: credentials
-                );
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              claims,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
 
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
-        // Usamos a anotação "AllowAnonymous" para 
-        // ignorar a autenticação neste método, já que é ele quem fará isso
+
+
+
+        // Usamos essa anotação para ignorar a autenticação neste método, já que é ele quem fará isso  
         [AllowAnonymous]
         [HttpPost]
-        public IActionResult Login([FromBody] Usuario login)
+        public IActionResult Login([FromBody]Usuario login)
         {
-            // Definimos logo de cara como não autorizado
             IActionResult response = Unauthorized();
-
-            // Autenticamos o usuário da API
             var user = AuthenticateUser(login);
+
             if (user != null)
             {
                 var tokenString = GenerateJSONWebToken(user);
